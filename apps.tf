dd8429e7d39dd4438882c82f0af146c6717eb4d6
Cam Jackson <camjackson89@gmail.com>
2019-04-10 21:20:20 +1000

    Fix CORS origin ordering on S3 buckets

--- apps.tf
+++ apps.tf
@@ -3,14 +3,20 @@ locals {
     container = "https://${local.site_domain}",
     content = "https://content.${local.site_domain}",
     browse = "https://browse.${local.site_domain}",
-    order = "https://order.${local.site_domain}"
+    order = "https://order.${local.site_domain}",
   }
+  all_domains = [
+    "${local.apps["browse"]}",
+    "${local.apps["order"]}",
+    "${local.apps["content"]}",
+    "${local.apps["container"]}",
+  ]
 }
 
 module "container" {
   source = "./s3_site/"
   domain = "${replace(local.apps["container"], "https://", "")}"
-  all_domains = "${values(local.apps)}"
+  all_domains = "${local.all_domains}"
   hosted_zone_id = "${data.aws_route53_zone.hosted_zone.zone_id}"
   acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
 }
@@ -18,7 +24,7 @@ module "container" {
 module "static_content" {
   source = "./s3_site/"
   domain = "${replace(local.apps["content"], "https://", "")}"
-  all_domains = "${values(local.apps)}"
+  all_domains = "${local.all_domains}"
   hosted_zone_id = "${data.aws_route53_zone.hosted_zone.zone_id}"
   acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
 }
@@ -26,7 +32,7 @@ module "static_content" {
 module "restaurant_browse_app" {
   source = "./s3_site/"
   domain = "${replace(local.apps["browse"], "https://", "")}"
-  all_domains = "${values(local.apps)}"
+  all_domains = "${local.all_domains}"
   hosted_zone_id = "${data.aws_route53_zone.hosted_zone.zone_id}"
   acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
 }
@@ -34,7 +40,7 @@ module "restaurant_browse_app" {
 module "restaurant_order_app" {
   source = "./s3_site/"
   domain = "${replace(local.apps["order"], "https://", "")}"
-  all_domains = "${values(local.apps)}"
+  all_domains = "${local.all_domains}"
   hosted_zone_id = "${data.aws_route53_zone.hosted_zone.zone_id}"
   acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
 }
